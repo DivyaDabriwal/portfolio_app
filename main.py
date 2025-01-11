3424bb2d2854f021ed483634f8fa62defe5bc77b
DivyaDabriwal <88877921+DivyaDabriwal@users.noreply.github.com>
2025-01-12 01:04:15 +0530

    extracting csv through csv module of python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import csv
 
 st.set_page_config(layout="wide")
 
@@ -20,3 +21,25 @@ with col2:
     st.info(content)
 
 st.write('Below you can find some of the apps I have built in Python, Feel free to contact me!')
+
+csv_file_content = []
+with open('files/data.csv') as file:
+    csv_file = csv.DictReader(file, delimiter=';')
+    for i in csv_file:
+        csv_file_content.append(i)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    for row in csv_file_content[:10]:
+        st.header(row['title'])
+        st.image('files/images/' + row['image'])
+        st.write(row['description'])
+        st.link_button('Source Code', row['url'])
+
+with col4:
+    for row in csv_file_content[10:]:
+        st.header(row['title'])
+        st.image('files/images/' + row['image'])
+        st.write(row['description'])
+        st.link_button('Source Code', row['url'])
